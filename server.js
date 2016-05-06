3ed8a406734cef04d00f6a56c99c4df2e1c8600e
Ignacio Anaya <ignacio.anaya89@gmail.com>
2016-05-06 02:50:50 -0300

    refactoring server

--- server.js
+++ server.js
@@ -24,34 +24,11 @@ app.listen(PORT, function() {
 });
 
 function serverHandler(req, res) {
-     var filePath = '.' + req.url;
-    if (filePath == './')
-        filePath = './index.html';
+  var filePath = '.' + req.url;
+  filePath = filePath === './' ? './index.html' : filePath;
 
-    var extname = path.extname(filePath);
-    var contentType = 'text/html';
-    switch (extname) {
-        case '.js':
-            contentType = 'text/javascript';
-            break;
-        case '.css':
-            contentType = 'text/css';
-            break;
-        case '.json':
-            contentType = 'application/json';
-            break;
-        case '.png':
-            contentType = 'image/png';
-            break;      
-        case '.jpg':
-            contentType = 'image/jpg';
-            break;
-        case '.wav':
-            contentType = 'audio/wav';
-            break;
-    }
-
-
+  var contentType = getContenType(filePath);
+  
 	fs.readFile(__dirname + '/public/' + filePath, function(err, data) {
 		if (err) {
 			console.log(err);
@@ -62,6 +39,34 @@ function serverHandler(req, res) {
 		res.writeHead(200,  { 'Content-Type': contentType });
 		res.end(data);
 	});
+}
+
+function getContentType(filePath) {
+  var extname = path.extname(filePath);
+  var contentType = 'text/html';
+
+  switch (extname) {
+    case '.js':
+    contentType = 'text/javascript';
+    break;
+    case '.css':
+    contentType = 'text/css';
+    break;
+    case '.json':
+    contentType = 'application/json';
+    break;
+    case '.png':
+    contentType = 'image/png';
+    break;      
+    case '.jpg':
+    contentType = 'image/jpg';
+    break;
+    case '.wav':
+    contentType = 'audio/wav';
+    break;
+  }
+
+  return contentType;
 }
 
 io.sockets.on('connection', (socket) => {
